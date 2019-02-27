51c4a0160edabe8d205f954276b2033f2aa54126
Til Blechschmidt <til@blechschmidt.de>
2019-02-27 16:15:59 +0100

    Adding docker and file watch support

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+watch() {
+    fswatch src | xargs -L 1 ./build.sh build
+}
 
 build() {
     pandoc src/main.md  \
@@ -20,8 +23,35 @@ spellcheck() {
         -o out/main.txt
 }
 
+help() {
+    cat << EOF
+Markdown typesetting tool.
+
+    usage:
+        build.sh [--docker][--help] command
+
+    parameters:
+        --docker        run inside docker container.
+        --help          print this.
+
+    commands:
+        build           typeset src/main.md -> out/main.pdf
+        spellcheck      typeset src/main.md -> out/main.txt
+        watch           watch src and execute build on file change.
+
+    examples:
+        build.sh watch
+        build.sh --docker build
+
+    Report bugs to: https://github.com/TexNAK/TFL-Template/issues
+EOF
+}
+
 mkdir -p out
 case $1 in
+    --help) help;;
+    --docker) BUILD_PARAMETERS="${@:2}" docker-compose -p latex_build -f .docker/docker-compose.yml up;;
     build) build;;
+    watch) watch;;
     spellcheck) spellcheck;;
 esac
